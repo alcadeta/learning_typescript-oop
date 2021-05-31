d74320917165a183e173b40be5e88eb1a6f9f298
Alican Demirtas <demirtasdev@hotmail.com>
2021-05-31 16:25:46 +0100

    Inheritance: The `Super` Keyword Overrides

---
+++ 2_Inheritance/4_TheSuperOverrides.ts
@@ -0,0 +1,27 @@
+// Base Class
+class Animal {
+    public lives = 1;
+
+    age: number; legs: number; name: string;
+    constructor(age:number, legs:number, name:string) {
+        this.age = age; this.legs = legs; this.name = name;
+    }
+
+    meow():string {
+        return "ROAR! ROAR! ROAR!";
+    }
+}
+
+// Implements / Matches Its Shape
+class Cat extends Animal {
+    lives = 9;
+
+    constructor(data: {age:number, legs:number, name:string}) {
+        super(data.age, data.legs, data.name);
+    }
+
+    meow():string {
+        super.meow();
+        return "MEOW! HISS! HISS!";
+    }
+}
