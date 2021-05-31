85c828651199e9508536b38f62549f2d49bdf427
Alican Demirtas <demirtasdev@hotmail.com>
2021-05-31 16:18:13 +0100

    Inheritance: The `Super` Keyword

---
+++ 2_Inheritance/3_TheSuperKeyword.ts
@@ -0,0 +1,33 @@
+// Base Class
+class Animal {
+    age: number; legs: number; name: string;
+    constructor(age:number, legs:number, name:string) {
+        this.age = age; this.legs = legs; this.name = name;
+    }
+}
+
+// Derived Class
+class Cat extends Animal {
+    constructor(data: {age:number, legs:number, name:string}) {
+        super(data.age, data.legs, data.name);
+    }
+
+    meow() {
+        return "MEOW! HISS! HISS!";
+    }
+}
+
+// Implementer Class
+class Dog implements Animal {
+    age: number; legs: number; name: string;
+    woof():string {
+        return "WOOF! WOOF! WOOF!";
+    }
+}
+
+
+const cat = new Cat({age: 19, legs: 4, name: "Baby"});
+const dog = new Dog();
+
+console.log(cat instanceof Animal); // TRUE becuase of `extends`
+console.log(dog instanceof Animal); // FALSE because of `implements`
