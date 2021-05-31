5424d21c76a164efc70e675c09858eff21060b14
Alican Demirtas <demirtasdev@hotmail.com>
2021-05-31 14:07:34 +0100

    Inheritance: Extending Classes

---
+++ 2_Inheritance/1_ExtendingClasses.ts
@@ -0,0 +1,30 @@
+// Base Class
+class Animal {
+    age: number; legs: number; name: string;
+    constructor(age:number, legs:number, name:string) {
+        this.age = age; this.legs = legs; this.name = name;
+    }
+}
+
+// Derived Classes
+class Dog extends Animal {
+    woof():string {
+        return "WOOF! WOOF! WOOF!";
+    }
+}
+const dog = new Dog(2, 4, "Gator");
+dog.age;
+dog.legs;
+dog.name;
+dog.woof();
+
+class Cat extends Animal {
+    meow() {
+        return "MEOW! HISS! HISS!";
+    }
+}
+const cat = new Cat(19, 4, "Baby");
+cat.age;
+cat.legs;
+cat.name;
+cat.meow();
