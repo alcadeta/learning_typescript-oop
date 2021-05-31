34520272b79dad8d87f6dfe2f2e60df33905deab
Alican Demirtas <demirtasdev@hotmail.com>
2021-05-31 12:25:27 +0100

    Classes: Classes vs Objects

---
+++ 1_Classes/1_ClassesVsObjects.ts
@@ -0,0 +1,9 @@
+// before classes
+const message1 = { title: undefined, message: undefined, id: undefined };
+const message2 = { title: undefined, message: undefined };
+const message3 = { title: undefined, message: undefined };
+
+// after classes
+class Message { title; message; id; }
+const message4 = new Message();
+const message5 = new Message();
