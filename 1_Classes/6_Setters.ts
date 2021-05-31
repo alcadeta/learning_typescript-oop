d0508159e5b16dcf5e659d892d97236758996dd9
Alican Demirtas <demirtasdev@hotmail.com>
2021-05-31 13:51:13 +0100

    Classes: Setters

---
+++ 1_Classes/6_Setters.ts
@@ -0,0 +1,41 @@
+class Message {
+    title:string;
+    message:string;
+    // here
+    private _isSent:boolean;
+    set isSent(value:boolean) {
+        if (value == true) {
+            this.deliveryDate = new Date();
+        }
+        this._isSent = value;
+    };
+    get isSent():boolean {
+        return this._isSent;
+    }
+    deliveryDate:Date;
+
+    constructor(title:string, message:string) {
+        this.title = title;
+        this.message = message;
+        this.isSent = false
+    }
+
+    get messageStatus():string {
+        const sentMessage = this.isSent ? "Has been sent."
+            : "Has not been sent.";
+        return `${this.message} | ${sentMessage}`
+    }
+
+    previewMessage():string {
+        return this.message.slice(0, 10).concat("...");
+    }
+}
+
+const message = new Message(
+    'New Course! Just £9.99!',
+    'Check out our latest course on OOP with TypeScript!'
+);
+
+message.previewMessage();
+message.messageStatus;
+message.isSent = true;
